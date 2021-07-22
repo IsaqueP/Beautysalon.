3af336bacfa0f56b823c72b110b774100f1cc216
IsaqueP <isaquepinheiro2002@gmail.com>
2021-07-22 09:44:40 -0300

    add images and fonts

---
+++ main.js
@@ -0,0 +1,18 @@
+// abre e fecha menu
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for (const element of toggle) {
+    element.addEventListener('click', function () {
+         nav.classList.toggle('show')
+    })
+}
+
+// esconder menu ao clicar em um item
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links){
+    link.addEventListener('click', function(){
+        nav.classList.remove('show')
+    })
+}
